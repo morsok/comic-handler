34876470ba86f209f7a8b223c775e60fd4fba873
Alexandre Fauquez <alexandre.fauquez@renault.com>
2023-01-21 16:59:33 +0100

    feat(backend): :tada: serve the angular app by default or static ressources

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+  "path"
+  "path/filepath"
+
+  "github.com/gin-gonic/gin"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+  router := gin.Default()
+  router.NoRoute(func(c *gin.Context) {
+    dir, file := path.Split(c.Request.RequestURI)
+    ext := filepath.Ext(file)
+    if file == "" || ext == "" {
+      c.File("./static/index.html")
+      } else {
+        c.File("./static/" + path.Join(dir, file))
+      }
+    })
+  router.Run("localhost:8080")
 }
